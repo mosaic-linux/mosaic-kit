9d2512c0c87b752ef3f93b181f384ce0128334c6
kieran <40911271+kieroid@users.noreply.github.com>
2024-05-27 20:36:16 +0200

    add license file

---
+++ kit.c
@@ -0,0 +1,18 @@
+/* See LICENSE file for copyright and license details.
+ *
+ * Mosaic Kit is designed to simply compile and install all of the
+ * software outlined in the README.md file. Not only that, but it
+ * also installs all of the scripts required to get the software outlined 
+ * working.
+ *
+ * The paths are defined in config.mk and the process of compiling is defined
+ * within the Makefile itself.
+ *
+ * To understand the process of compiling all the software, please look ahead
+ * to the main() function.
+ */
+
+void main() {
+	int i;
+}
+
